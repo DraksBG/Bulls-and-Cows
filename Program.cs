6d5137aae638f481ca735d02581f988f3024e536
Dimitar Krastanov <DraksBG@users.noreply.github.com>
2019-07-01 13:02:26 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulsAndCows
+{
+    class Program
+    {
+        static int RandomNum()
+        {
+            Random r = new Random();
+            int n = r.Next(1000, 9999);
+
+            return n;
+        }
+        static void Bulls(int numbers, int input, out int bulls)
+        {
+            char[] inputString = input.ToString().ToCharArray();
+            char[] secretNum = numbers.ToString().ToCharArray();
+
+            bulls = 0;
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (inputString[i] == secretNum[i])
+                {
+                    bulls++;
+                }
+
+            }
+            Console.WriteLine(bulls);
+        }
+        static void Cows(int number, int input, out int cows)
+        {
+            char[] inputString = input.ToString().ToCharArray();
+            char[] secretNum = number.ToString().ToCharArray();
+            cows = 0;
+            for (int i = 0; i < secretNum.Length; i++)
+            {
+                for (int j = 0; j < inputString.Length; j++)
+                {
+                    if (j != i)
+                    {
+                        if (inputString[j] == secretNum[i])
+                        {
+                            cows++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(cows);
+        }
+
+        static void Main(string[] args)
+        {
+            int Remember = RandomNum();
+            
+            while (true)
+            {
+                int input = int.Parse(Console.ReadLine());
+              
+                Console.WriteLine(Remember);
+                Console.WriteLine("Numbers of Bulls is : ");
+                Bulls(Remember, input, out int bulls);
+                Console.WriteLine();
+                Console.WriteLine("Numbers of Cows is :");
+                Cows(Remember, input, out int cows);
+
+                if (bulls == 4)
+                {
+                    break;
+                }
+            }
+            
+
+
+
+
+
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BulsAndCows")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BulsAndCows")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ed6b3c6e-82ec-449d-b78c-326bcd3e3b7e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
